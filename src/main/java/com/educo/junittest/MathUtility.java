4ef7079c52ae44a5badd747d5fe23156ff511ea1
Ashutosh Pandey <ashutosh@keriton.com>
2019-11-18 11:48:50 +0530

    First commit

---
+++ src/main/java/com/educo/junittest/MathUtility.java
@@ -0,0 +1,11 @@
+package com.educo.junittest;
+
+public class MathUtility {
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int square(int n) {
+		return n*n;
+	}
+}
---
+++ src/test/java/com/educo/junittest/MathUtilityTest.java
@@ -0,0 +1,27 @@
+package com.educo.junittest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MathUtilityTest {
+	
+	private MathUtility utility;
+	
+	@Before
+	public void initialize() {
+		utility = new MathUtility();
+	}
+
+	@Test
+	public void testSquare() {
+		int sq = utility.square(4);
+		Assert.assertEquals(16, sq);
+	}
+	
+	@Test
+	public void testAdd() {
+		int sum = utility.add(4, 5);
+		Assert.assertEquals(9, sum);
+	}
+}
