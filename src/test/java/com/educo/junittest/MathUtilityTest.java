a47505884e93a1036ac461ce15209b4fb7fd9aa9
Ashutosh Pandey <ashutosh@keriton.com>
2019-11-18 18:09:28 +0530

    Updated test

--- src/test/java/com/educo/junittest/MathUtilityTest.java
+++ src/test/java/com/educo/junittest/MathUtilityTest.java
@@ -21,7 +21,7 @@ public class MathUtilityTest {
 	
 	@Test
 	public void testAdd() {
-		int sum = utility.add(16, 15);
-		Assert.assertEquals(31, sum);
+		int sum = utility.add(6, 15);
+		Assert.assertEquals(21, sum);
 	}
 }
